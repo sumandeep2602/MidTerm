07ed56060a1a3a18fa74cf6cc7901e2d5247aeaf
Sumandeep kaur <ksumanmaan3952@gmail.com>
2019-09-19 11:05:46 -0400

    red R1 COMMIT

---
+++ LabTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabTest2
+//
+//  Created by MacStudent on 2019-09-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
